7526c5538a68bfc8863e0bbc3d7126397191fdf9
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-11-28 16:45:57 +0700

    feat: tailwind & chartjs

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,5 @@
+// import Chart from "chart.js/auto";
+
 const url = "https://jsonplaceholder.typicode.com/users";
 axios
   .get(url)
@@ -37,3 +39,41 @@ async function getPokemonWithAbilities() {
   }
 }
 getPokemonWithAbilities();
+
+const dataPenjualan = [
+  { year: 2010, count: 10 },
+  { year: 2011, count: 20 },
+  { year: 2012, count: 15 },
+  { year: 2013, count: 25 },
+  { year: 2014, count: 22 },
+  { year: 2015, count: 30 },
+  { year: 2016, count: 28 },
+];
+
+(async function (dataInput) {
+  const canvas = document.querySelector("#grafik");
+  const labels = dataInput.map((datum) => datum.year);
+  const data = dataInput.map((datum) => datum.count);
+  new Chart(canvas, {
+    type: "scatter",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: "Penjualan per Tahun",
+          data,
+        },
+      ],
+    },
+  });
+})(dataPenjualan);
+
+axios
+  .get("https://api.themoviedb.org/3/movie/now_playing", {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2E0MjU3MWIwYTJhMTI3MzhkMGZhYTRjMjQzODRjNCIsIm5iZiI6MTc0MTA3MTk1Mi4xNzEsInN1YiI6IjY3YzZhNjUwZGQ0NmJkMmZkMzkwN2FmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yEjyd9DnV8rUU3eQerJ_kclflblwGpSt6MLIpux5_YE",
+    },
+  })
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err));
